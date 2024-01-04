e2342a5148c5767026b251802d56f7b2f6b91312
raisew <276871162@qq.com>
2024-01-04 14:12:19 +0800

    update search

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 import { docsAuto } from "./utils/autodoc";
 
+
 let { navBar, sideBar } = docsAuto();
 
 // https://vitepress.dev/reference/site-config
@@ -18,7 +19,6 @@ export default defineConfig({
     math: false,
     lineNumbers: true,
     codeTransformers: [
-      // We use `[!!code` in demo to prevent transformation, here we revert it back.
       {
         postprocess(code) {
           return code.replace(/\[\!\!code/g, "[!code");
@@ -42,11 +42,8 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     logo: "/logo.png",
     siteTitle: "hongyangwu",
-    // search: createSearch(),
     // algolia: algoliaJSON(),
-    search: {
-      provider: "local",
-    },
+    search: createSearch(),
     nav: navBar,
     sidebar: sideBar,
 
@@ -111,55 +108,32 @@ function algoliaJSON() {
 
 function createSearch() {
   return {
-    // provider: "local",
-    provider: "algolia",
-    options: {
-      appId: "RMVAHTZ5LL",
-      apiKey: "96dbb8e7c314f86172f3108ef9338d89",
-      indexName: "wedoc",
-      // locales: {
-      //   zh: {
-      placeholder: "搜索文档",
+    provider: "local",
+    options:{
+      _render(src, env, md) {
+        const html = md.render(src, env)
+        if (env.frontmatter?.title)
+          return md.render(`# ${env.frontmatter.title}`) + html
+        return html
+      },
       translations: {
         button: {
-          buttonText: "搜索文档",
-          buttonAriaLabel: "搜索文档",
+          buttonText: '搜索文档',
+          buttonAriaLabel: '搜索文档'
         },
         modal: {
-          searchBox: {
-            resetButtonTitle: "清除查询条件",
-            resetButtonAriaLabel: "清除查询条件",
-            cancelButtonText: "取消",
-            cancelButtonAriaLabel: "取消",
-          },
-          startScreen: {
-            recentSearchesTitle: "搜索历史",
-            noRecentSearchesText: "没有搜索历史",
-            saveRecentSearchButtonTitle: "保存至搜索历史",
-            removeRecentSearchButtonTitle: "从搜索历史中移除",
-            favoriteSearchesTitle: "收藏",
-            removeFavoriteSearchButtonTitle: "从收藏中移除",
-          },
-          errorScreen: {
-            titleText: "无法获取结果",
-            helpText: "你可能需要检查你的网络连接",
-          },
+          displayDetails: '显示详细列表',
+          resetButtonTitle: '重置搜索',
+          backButtonTitle: '关闭搜索',
+          noResultsText: '无法找到相关结果',
           footer: {
-            selectText: "选择",
-            navigateText: "切换",
-            closeText: "关闭",
-            searchByText: "搜索提供者",
-          },
-          noResultsScreen: {
-            noResultsText: "无法找到相关结果",
-            suggestedQueryText: "你可以尝试查询",
-            reportMissingResultsText: "你认为该查询应该有结果？",
-            reportMissingResultsLinkText: "点击反馈",
-          },
-        },
-      },
-      //   },
-      // },
-    },
+            selectText: '选择',
+            navigateText: '切换',
+            closeText: '关闭',
+          }
+        }
+      }
+    }
+    
   };
 }
